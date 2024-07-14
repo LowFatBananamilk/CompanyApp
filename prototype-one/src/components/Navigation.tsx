203047c4ba054295f6aacb200ea5fb30e286be2a
김승원 <bananamilk420@gmail.com>
2024-07-14 16:33:14 +0900

    feat: :lipstick: Added CSS for nav-bar to have full width dropdown

--- prototype-one/src/components/Navigation.tsx
+++ prototype-one/src/components/Navigation.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
+
+import './components.css'
  
 const Navigation = () => {
     return (
