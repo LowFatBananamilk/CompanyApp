1d89dff5fc9082ec17af87724a8492c02284bf0a
김승원 <bananamilk420@gmail.com>
2024-07-16 21:24:46 +0900

    feat(Editor): :sparkles: React Quil component 생성

---
+++ prototype-one/src/components/Editor.tsx
@@ -0,0 +1,11 @@
+import React, { useMemo, useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+function Editor() {
+  return (
+    <ReactQuill theme="snow" />
+  );
+}
+
+export default Editor;
