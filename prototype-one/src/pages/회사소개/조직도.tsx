c7f9a6778faba4da056b43a28aa5bd7ea56e3171
Calcifer Kim <calcifer.kim@gmail.com>
2024-07-17 02:12:45 +0900

    조직도 svg, css, html

--- prototype-one/src/pages/회사소개/조직도.tsx
+++ prototype-one/src/pages/회사소개/조직도.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import '../Font&Color.css'
+import './조직도.css'
+import 조직 from '../../조직도.svg';
+
 
 function 조직도() {
   return (
-    <h1>조직도</h1>
+  <div>
+    <br></br>
+    <br></br>
+    <h1 className="bold logocolor2 center big">조직도</h1>
+    <div className='start'>
+    </div>
+    
+    <div className="mainone">
+      <img src={조직} className='jojik'></img>
+    </div>
+  </div>
   );
 }
 
