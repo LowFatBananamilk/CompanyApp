6d48d65c7ba93e944b152338816ff5b87e9e0132
Calcifer Kim <calcifer.kim@gmail.com>
2024-07-17 01:34:22 +0900

    car icons for 주고객사 and finished 회사소개 almost

--- prototype-one/src/pages/회사소개/회사소개.tsx
+++ prototype-one/src/pages/회사소개/회사소개.tsx
@@ -4,6 +4,7 @@ import './회사소개.css';
 import company from '../../company.svg';
 import money from '../../money.svg';
 import worker from '../../worker.svg';
+import car from '../../car.svg';
 
 function 회사소개() {
   return (
@@ -14,14 +15,18 @@ function 회사소개() {
 
     <div className="intro">
       <h1 className="bold logocolor3">HANJIN PL</h1>
+      <hr></hr>
       <h4>한진PL은 빛입니다.</h4>
       <h6>뭐시기 뭐시기 했습니다.</h6>
       <h6 className='bold'>중요!!</h6>
       <h6 className='logocolor3'>파랑!!</h6>
+      <hr></hr>
     </div>
     
+
     <div className="CEO bg1">
       <h1 className='bold logocolor3'>CEO 인사말</h1>
+      <hr></hr>
       <h4>한진PL은 빛입니다.</h4>
       <h6>뭐시기 뭐시기 했습니다.</h6>
       <h6 className='bold'>중요!!</h6>
@@ -29,40 +34,79 @@ function 회사소개() {
     </div>
 
     <div className="info">
-      <table>
-        <tr>
-          <th>설립일</th>
-          <th>매출액</th>
-          <th>종업원수</th>
+      <h1 className="bold logocolor3">기업정보</h1>
+      <table className="table1">
+        <tr className='tr1'>
+          <th className='th1'>설립일</th>
+          <th className='th1'>매출액</th>
+          <th className='th1'>종업원수</th>
         </tr>
-        <tr>
-          <td>
+        <tr className='th1'>
+          <td className='th1'>
             <img src={company} className='icon'></img>
           </td>
-          <td>
+          <td className='th1'>
             <img src={money} className='icon'></img>
           </td>
-          <td>
+          <td className='th1'>
             <img src={worker} className='icon'></img>
           </td>
         </tr>
-        <tr>
-          <td>
+        <tr className='th1'>
+          <td className='th1'>
             <h5>부제목</h5>
             <h6>부가설명</h6>
           </td>
-          <td>
+          <td className='th1'>
             <h5>부제목</h5>
             <h6>부가설명</h6>
           </td>
-          <td>
+          <td className='th1'>
             <h5>부제목</h5>
             <h6>부가설명</h6>
           </td>
         </tr>
       </table>
     </div>
+    
+    <div className='info2'>
+      <table className="table2">
+      <tr className='tr2'>
+          <th className='th2'>회사명</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+        <tr className='tr2'>
+          <th className='th2'>대표이사</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+        <tr className='tr2'>
+          <th className='th2'>법인출범</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+        <tr className='tr2'>
+          <th className='th2'>주생산품</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+        <tr className='tr2'>
+          <th className='th2'>주요사업</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+        <tr className='tr2'>
+          <th className='th2'>주소</th>
+          <td className='td2'>이름 및 설명</td>
+        </tr>
+      </table>
+    </div>
 
+    <div className='others'>
+    <h1 className="bold logocolor3">주관계사</h1>
+    <hr></hr>
+    <img src={car} className='companies'></img>
+    <img src={car} className='companies'></img>
+    <img src={car} className='companies'></img>
+    <img src={car} className='companies'></img>
+    <h5 className="bold">현대, 뭐시기, 뭐시기</h5>
+    </div>
   </div>
 
   );
