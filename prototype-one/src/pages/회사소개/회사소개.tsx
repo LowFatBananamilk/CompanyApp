b7a74abc35dcb771c00d9518e345109b1b4b625b
Calcifer Kim <calcifer.kim@gmail.com>
2024-07-16 21:42:33 +0900

    added svg for 회사소개, created general css for font and color, edited div blocks for 회사소개 css and html both

--- prototype-one/src/pages/회사소개/회사소개.tsx
+++ prototype-one/src/pages/회사소개/회사소개.tsx
@@ -1,63 +1,61 @@
 import React from 'react';
+import '../Font&Color.css'
 import './회사소개.css'; 
+import company from '../../company.svg';
+import money from '../../money.svg';
+import worker from '../../worker.svg';
 
 function 회사소개() {
   return (
-    <div className="General">
-      <hr></hr>
-      <br></br>
-      <h1 className='header'>회사개요</h1>
-      <br></br>
-      <div className='explanation'>
-        <br></br>
-        <div>
-        <br></br>
-          <table className="table1">
-            <tr>
-              <th>대표이사</th>
-              <td>류지민</td>
-            </tr>
-            <tr>
-              <th>법인출범</th>
-              <td>2008년 5월 1일</td>
-            </tr>
-            <tr>
-              <th>종업원</th>
-              <td>374명</td>
-            </tr>
-            <tr>
-              <th>매출</th>
-              <td>579억 (2021년)</td>
-            </tr>
-            <tr>
-              <th>주생산품</th>
-              <td>자동차 도어트림 내장재/크러시패드 내장재
-                <ul><li className="arrow">고압사출, IMG 진공성형, 수작업 감싸기 및 조립 </li></ul>
-              </td>
-            </tr>
-            <tr>
-              <th>주소</th>
-              <td>경상북도 경주시 외동읍 제내 2공단길</td>
-            </tr>
-          </table>
-          <br></br>
-        </div>
-      
-      <div>
-      
-      </div>
+  <div>
+    <div className="photodiv bg5">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/M%C3%BCnster%2C_LVM%2C_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg/1200px-M%C3%BCnster%2C_LVM%2C_B%C3%BCrogeb%C3%A4ude_--_2013_--_5149-51.jpg"></img>
+    </div>
 
-      <div>
-
-      </div>
-
-      <div>
-
-      </div>
-      </div>
-      
+    <div className="intro">
+      <h1 className="bold logocolor3">HANJIN PL</h1>
+      <h4>한진PL은 빛입니다.</h4>
+      <h6>뭐시기 뭐시기 했습니다.</h6>
+      <h6 className='bold'>중요!!</h6>
+      <h6 className='logocolor3'>파랑!!</h6>
     </div>
     
+    <div className="CEO bg1">
+      <h1 className='bold logocolor3'>CEO 인사말</h1>
+      <h4>한진PL은 빛입니다.</h4>
+      <h6>뭐시기 뭐시기 했습니다.</h6>
+      <h6 className='bold'>중요!!</h6>
+      <h6 className='logocolor3'>파랑!!</h6>
+    </div>
+
+    <div className="info">
+      <table>
+        <tr>
+          <th>설립일</th>
+          <th>매출액</th>
+          <th>종업원수</th>
+        </tr>
+        <tr>
+          <td>
+            <img src={company}></img>
+            <h5>년월일</h5>
+            <h6>부가설명</h6>
+          </td>
+          <td>
+            <img src={money}></img>
+            <h5>년월일</h5>
+            <h6>부가설명</h6>
+          </td>
+          <td>
+            <img src={worker}></img>
+            <h5>년월일</h5>
+            <h6>부가설명</h6>
+          </td>
+        </tr>
+      </table>
+    </div>
+
+  </div>
 
   );
 }
