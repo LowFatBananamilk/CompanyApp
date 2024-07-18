615b44f5071718b018d6c3489d4c095a92fe4fd9
김승원 <bananamilk420@gmail.com>
2024-07-18 23:47:06 +0900

    fix: :bug: Use different port for backend as the frontend uses the same one.
    port 3000 is unofficially reservered of React App. next available port is 3002

--- backend/index.ts
+++ backend/index.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 
 
 const app: Express = express();
-const port = 3000;
+const port = 3002;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
