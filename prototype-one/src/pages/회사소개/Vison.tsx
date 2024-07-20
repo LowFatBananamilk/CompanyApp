6616a647c3df4bdc0975ee3467128287324464f2
Calcifer Kim <calcifer.kim@gmail.com>
2024-07-21 00:03:14 +0900

    gave basic layout for vision n 매출현황

--- prototype-one/src/pages/회사소개/Vison.tsx
+++ prototype-one/src/pages/회사소개/Vison.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import './Vision.css'
+import vision1 from '../../vision1.png'
 
 function Vison() {
   return (
-    <h1>Vison</h1>
+  <div>
+    <div className='thumbnail1'>
+      <h1 className='thumbnailtext'>Vision</h1>
+    </div>
+    <h1 className="header">Vision</h1>
+    <div className='start'>
+    </div>
+    <div>
+      <img src={vision1} className='vision1'></img>
+    </div>
+
+    <div>
+
+    </div>
+
+  </div>
   );
 }
 
--- prototype-one/src/pages/회사소개/매출현황.tsx
+++ prototype-one/src/pages/회사소개/매출현황.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import status from '../../매출현황.png'
+import './매출현황.css'
 
 function 매출현황() {
   return (
-    <h1>매출현황</h1>
+  <div>
+    <div className='thumbnail1'>
+      <h1 className='thumbnailtext'>매출현황</h1>
+    </div>
+    <h1 className="header">매출현황</h1>
+    <div className='start'>
+    </div>
+    <div>
+      <img src={status} className='status'></img>
+    </div>
+  </div>
   );
 }
 
