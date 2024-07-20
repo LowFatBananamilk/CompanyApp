a66ea32ca9255800ae775609e6853ba0207c8283
김승원 <bananamilk420@gmail.com>
2024-07-21 00:19:57 +0900

    feat(Editor): :sparkles: Added get page by location feature

--- backend/page/pageController.ts
+++ backend/page/pageController.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import { PageService as Service } from './pageService';
+import Page from './pageInterface'
 
 export const PageController = Router();
 
-PageController.get("/:id", async (req, res) => {
-    const page = await Service.GetByID(Number(req.params.id))
-    return res.json(page)
+PageController.get('/', async (req, res) => {
+    let page: Page | null = null
+    if (req.query.id)
+        page = await Service.GetByID(Number(req.query.id));
+    else if (req.query.location)
+        page = await Service.GetByLocation(String(req.query.location));
+
+    if (page == null)
+        return res.sendStatus(404);
+    return res.json(page);
+
 });
--- backend/page/pageService.ts
+++ backend/page/pageService.ts
@@ -4,7 +4,12 @@ import Page from './pageInterface'
 
 export class PageService {
     static async GetByID(ID: number) {
-        const [rows, _]: [Page[], FieldPacket[]] = await connection.promise().query('SELECT * FROM page WHERE ID = ?', ID);
+        const [rows, _]: [Page[], FieldPacket[]] = await connection.promise().query('SELECT * FROM page WHERE id = ?', ID);
+        return rows[0];
+    }
+
+    static async GetByLocation(location: string) {
+        const [rows, _]: [Page[], FieldPacket[]] = await connection.promise().query('SELECT * FROM page WHERE location = ?', location);
         return rows[0];
     }
 }
