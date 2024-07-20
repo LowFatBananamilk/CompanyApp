95575fc2ecbcc4bcff300c596fddf90b256eb498
Calcifer Kim <calcifer.kim@gmail.com>
2024-07-20 23:20:44 +0900

    minor class error fixed

--- prototype-one/src/pages/회사소개/경영목표.tsx
+++ prototype-one/src/pages/회사소개/경영목표.tsx
@@ -25,63 +25,63 @@ function 경영목표() {
     </div>
     <div className='goaldetails'>
       <table>
-        <tr>
-          <td><img src={increase} className='icons'></img></td>
-          <td className='detail1'>매출액 증대를 위한 솔루션 구축</td>
-          <td className='detail2'>
-            <ul>
-              <li>
+        <tr className='tabler'>
+          <td className='tabled'><img src={increase} className='icons'></img></td>
+          <td className='detail1 tabled'>매출액 증대를 위한 솔루션 구축</td>
+          <td className='detail2 tabled'>
+            <ul className='ulist'>
+              <li className='list'>
                 2021: 579억
               </li>
-              <li>
+              <li className='list'>
                 02022: 604억
               </li>
             </ul>
           </td>
         </tr>
-        <tr>
-          <td><img src={quality} className='icons'></img></td>
-          <td className='detail1'>품질목표</td>
-          <td className='detail2'>
-            <ul>
-              <li>
+        <tr className='tabler'>
+          <td className='tabled'><img src={quality} className='icons'></img></td>
+          <td className='detail1 tabled'>품질목표</td>
+          <td className='detail2 tabled'>
+            <ul className='ulist'>
+              <li className='list'>
               HMC 사출 SQ Level “G”
               </li>
-              <li>
+              <li className='list'>
               SQ Level “G” 유지 
               </li>
             </ul>
           </td>
         </tr>
-        <tr>
-          <td><img src={safety} className='icons'></img></td>
-          <td className='detail1'>안전관리 시스템</td>
-          <td className='detail2'>
-            <ul>
-              <li>
+        <tr className='tabler'>
+          <td className='tabled'><img src={safety} className='icons'></img></td>
+          <td className='detail1 tabled'>안전관리 시스템</td>
+          <td className='detail2 tabled'>
+            <ul className='ulist'>
+              <li className='list'>
                 폭설, 폭우 등 기상재해 조기대응시스템 운영
               </li>
-              <li>
+              <li className='list'>
               지진 발생시 지정된 대피장소 이동/ 사전 메뉴얼 교육
               </li>
-              <li>
+              <li className='list'>
               화재시 정기 교육된 소방조직으로 초기 진압
               </li>
             </ul>
             </td>
         </tr>
-        <tr>
-          <td><img src={A} className='icons'></img></td>
-          <td className='detail1'>기업경쟁력 강화</td>
-          <td className='detail2'>
+        <tr className='tabler'>
+          <td className='tabled'><img src={A} className='icons'></img></td>
+          <td className='detail1 tabled'>기업경쟁력 강화</td>
+          <td className='detail2 tabled'>
             <ul>
-              <li>
+              <li className='list'>
               사출공정의 스마트 팩토리 도입으로 생산 및 품질 향상
               </li>
-              <li>
+              <li className='list'>
               사출부터 조립공정으로 총괄적인 생산 운영 능력 강화
               </li>
-              <li>
+              <li className='list'>
               상품성 개선/고급화의 신기술 개발로 가격경쟁력 강화
               </li>
             </ul>
