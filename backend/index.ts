eb5d982d0cdcb72ca8c1af68c7fe8eb43a13cac6
김승원 <bananamilk420@gmail.com>
2024-07-27 17:10:41 +0900

    fix(production): :bug: Ensure frontend is reachable from anywhere

--- backend/index.ts
+++ backend/index.ts
@@ -8,6 +8,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
