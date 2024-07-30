ac86dfbb27bd12e6068871b72a89566619f721f9
김승원 <bananamilk420@gmail.com>
2024-07-30 22:12:39 +0900

    feat: :sparkles: 메인 페이지에 carousel 추가
    현 메뉴 개수만큼 스라이드 지정 및 메뉴 이름 및 링크 지정

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,8 +1,71 @@
-import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Image from './logo.png'
 
 function Main() {
   return (
-    <h1>Main</h1>
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <a href="#/회사소개/회사정보">
+            <img
+              src={Image}
+              alt="Logo"
+              style={{ height: "800px", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            <Carousel.Caption>
+              <h3>회사소개</h3>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a href='#/esg/ESG경영'>
+            <img
+              src={Image}
+              alt="Logo"
+              style={{ height: "800px", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            <Carousel.Caption>
+              <h3>ESG</h3>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a href='#/인재채용/careerinfo'>
+            <img
+              src={Image}
+              alt="Logo"
+              style={{ height: "800px", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            <Carousel.Caption>
+              <h3>인재채용</h3>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a href='#/사업장/관계사'>
+            <img
+              src={Image}
+              alt="Logo"
+              style={{ height: "800px", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            <Carousel.Caption>
+              <h3>사업장</h3>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <a href='#/제품/제조공정'>
+            <img
+              src={Image}
+              alt="Logo"
+              style={{ height: "800px", display: "block", marginLeft: "auto", marginRight: "auto" }} />
+            <Carousel.Caption>
+              <h3>제품</h3>
+            </Carousel.Caption>
+          </a>
+        </Carousel.Item>
+      </Carousel>
+    </>
   );
 }
 
