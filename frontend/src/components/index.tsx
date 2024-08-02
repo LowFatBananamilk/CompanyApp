6bec384ddd588e9bea4205b0d5f2b3368a3bff3f
김승원 <bananamilk420@gmail.com>
2024-08-02 23:25:41 +0900

    fix: :ambulance: 카카오 지도 API 문제로 인한 hot fix
    카카오 API 사용 시 플랫폼 사이트 도메인 설정에 문제가 있는 것 같음. localhost를 사용하는 개발 환경에서는 지도가 사용이 안되고 있음. 배포 환경에서는 문제 없을 것으로 예상. 임시적으로 Google 지도를 사용

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -2,4 +2,5 @@ export { default as Navigation } from "./Navigation";
 export { default as Editor } from "./Editor";
 export { default as Footer } from './Footer';
 export { default as NavCarousel } from './NavCarousel';
-export { default as Map } from './Map';
+// export { default as Map } from './Map';
+export { default as Map } from './GoogleMap';
