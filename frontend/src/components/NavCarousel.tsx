7acb6eec029273be833ffc84cec6b72b23585846
Calcifer Kim <calcifer.kim@gmail.com>
2024-08-08 17:17:58 +0900

    finished carousel and started on 인증서

--- frontend/src/components/NavCarousel.tsx
+++ frontend/src/components/NavCarousel.tsx
@@ -21,7 +21,7 @@ export default function NavCarousel() {
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2018/03/10/12/00/teamwork-3213924_1280.jpg"></img>
             <Carousel.Caption>
               <h3 className='CIH'>ESG</h3>
-              <p className='CIP'>CEO 인사말, 기업정보, CI소개, 오시는길</p>
+              <p className='CIP'>ESG 경영</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -31,7 +31,7 @@ export default function NavCarousel() {
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2024/06/09/14/44/businessman-8818855_1280.jpg"></img>
             <Carousel.Caption>
               <h3 className='CIH'>인재채용</h3>
-              <p className='CIP'>Nualla vitae</p>
+              <p className='CIP'>CAREER INFO, Q&A, 채용링크</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -41,7 +41,7 @@ export default function NavCarousel() {
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2013/06/10/09/23/desert-123978_1280.jpg"></img>
             <Carousel.Caption>
               <h3 className='CIH'>사업장</h3>
-              <p className='CIP'>Nualla vitae</p>
+              <p className='CIP'>관계사</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -51,7 +51,7 @@ export default function NavCarousel() {
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2018/08/31/18/51/mercedes-benz-3645351_1280.jpg"></img>
             <Carousel.Caption>
               <h3 className='CIH'>제품</h3>
-              <p className='CIP'>Nualla vitae</p>
+              <p className='CIP'>제조공정, 설비현황, 제품소개</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
--- frontend/src/pages/회사소개/인증서현황.tsx
+++ frontend/src/pages/회사소개/인증서현황.tsx
@@ -2,10 +2,41 @@ import React from 'react';
 import '../Font&Color.css';
 import '../thumbnail&details.css';
 import '../divs.css';
+import first from './인증서.png';
+import second from'./second.png';
+import './인증서현황.css';
 
 
 export default function 인증서현황() {
   return (
-    <>인증서현황</>
+    <div> 
+        <div>
+            <h1 className='header1'>인증서 현황</h1>
+            <div className='start'></div>
+        </div>
+
+        <div className='main'>
+            <div className='oneA'>
+                <h1 className='header2'>2009 ~ 2024</h1>
+                <hr></hr>
+            </div>
+            <br></br>
+              <table className='tabletbt'>
+                <tr className='tabletbtr'>
+                  <td className='tabletbth'>2023</td>
+                  <td className='tabletbth'>2024</td>
+                </tr>
+                <tr className='tabletbtr'>
+                  <td className='tabletbtd'>
+                    <img src={second} className='tabletbtimgA'></img>
+                  </td>
+                  <td className='tabletbtd'>
+                    <img src={first} className='tabletbtimgB'></img>
+                  </td>
+                </tr>
+              </table>
+            <br></br>
+        </div>
+    </div>
   );
 }
