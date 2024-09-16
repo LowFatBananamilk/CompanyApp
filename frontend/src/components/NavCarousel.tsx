135a5022cfdb7da0cbf4622420169b4f466908ee
Calcifer Kim <calcifer.kim@gmail.com>
2024-09-15 20:44:42 -0400

    changed color for nav carousel

--- frontend/src/components/NavCarousel.tsx
+++ frontend/src/components/NavCarousel.tsx
@@ -10,8 +10,8 @@ export default function NavCarousel() {
           <a href="#/회사소개/회사정보">
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2020/04/17/20/25/road-5056788_1280.jpg"></img>
             <Carousel.Caption>
-              <h3 className='CIH'>회사소개</h3>
-              <p className='CIP'>CEO 인사말, 기업정보, CI소개, 오시는길</p>
+              <h3 className='CIH2'>회사소개</h3>
+              <p className='CIP2'>CEO 인사말, 기업정보, CI소개, 오시는길</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -20,8 +20,8 @@ export default function NavCarousel() {
           <a href='#/esg/ESG경영'>
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2018/03/10/12/00/teamwork-3213924_1280.jpg"></img>
             <Carousel.Caption>
-              <h3 className='CIH'>ESG</h3>
-              <p className='CIP'>ESG 경영</p>
+              <h3 className='CIH2'>ESG</h3>
+              <p className='CIP2'>ESG 경영</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -30,8 +30,8 @@ export default function NavCarousel() {
           <a href="#/회사소개/회사정보">
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2024/06/09/14/44/businessman-8818855_1280.jpg"></img>
             <Carousel.Caption>
-              <h3 className='CIH'>인재채용</h3>
-              <p className='CIP'>CAREER INFO, Q&A, 채용링크</p>
+              <h3 className='CIH2'>인재채용</h3>
+              <p className='CIP2'>CAREER INFO, Q&A, 채용링크</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -40,8 +40,8 @@ export default function NavCarousel() {
           <a href="#/회사소개/회사정보">
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2013/06/10/09/23/desert-123978_1280.jpg"></img>
             <Carousel.Caption>
-              <h3 className='CIH'>사업장</h3>
-              <p className='CIP'>관계사</p>
+              <h3 className='CIH2'>사업장</h3>
+              <p className='CIP2'>관계사</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
@@ -50,8 +50,8 @@ export default function NavCarousel() {
           <a href="#/회사소개/회사정보">
             <img className='navCarousel' src="https://cdn.pixabay.com/photo/2018/08/31/18/51/mercedes-benz-3645351_1280.jpg"></img>
             <Carousel.Caption>
-              <h3 className='CIH'>제품</h3>
-              <p className='CIP logocolor1'>제조공정, 설비현황, 제품소개</p>
+              <h3 className='CIH2'>제품</h3>
+              <p className='CIP2'>제조공정, 설비현황, 제품소개</p>
             </Carousel.Caption>
           </a>
         </Carousel.Item>
