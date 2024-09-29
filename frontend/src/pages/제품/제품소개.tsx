8027c22d0e95a8e715e0b496cf00880412db6e54
김승원 <bananamilk420@gmail.com>
2024-09-29 03:17:59 +0000

    feat: :sparkles: 제품소개 페이지 - 버튼을 이용한 여러 컨텐츠 표시 샘플

--- frontend/src/pages/제품/제품소개.tsx
+++ frontend/src/pages/제품/제품소개.tsx
@@ -1,19 +1,59 @@
-import React from 'react';
-import '../Font&Color.css';
-import '../thumbnail&details.css';
-import '../divs.css';
+import React, { useState } from 'react';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import ToggleButton from 'react-bootstrap/ToggleButton';
 
 
 export default function 제품소개() {
-  return (
-    <div>
-        <div> 
-            <h1 className='header1'>제품소개</h1>
-            <div className='start'></div>
-        </div>
+  // Set tabIndex to 1 by default to show the first tab on load
+  const [radioValue, setRadioValue] = useState('1');
 
-        <div className='main'>
-        </div>
-    </div>
+  const radios = [
+    { name: '제품1', value: '1', style: { backgroundColor: 'blue' } },
+    { name: '제품2', value: '2', style: { backgroundColor: 'teal' } },
+    { name: '제품3', value: '3', style: { backgroundColor: 'aqua' } },
+  ];
+
+
+  return (
+    <>
+      <ButtonGroup className='button-group'>
+        {radios.map((radio, idx) => (
+          <ToggleButton
+            key={idx}
+            id={`radio-${idx}`}
+            type="radio"
+            name="radio"
+            value={radio.value}
+            checked={radioValue === radio.value}
+            onChange={(e) => setRadioValue(e.currentTarget.value)}
+          // style={radio.style}
+          >
+            {radio.name}
+          </ToggleButton>
+        ))}
+      </ButtonGroup>
+
+      {
+        radioValue === '1' && (
+          <>
+            <h3>1st Content</h3>
+          </>
+        )
+      }
+      {
+        radioValue === '2' && (
+          <>
+            <h3>2nd Content</h3>
+          </>
+        )
+      }
+      {
+        radioValue === '3' && (
+          <>
+            <h3>3rd Content</h3>
+          </>
+        )
+      }
+    </>
   );
 }
