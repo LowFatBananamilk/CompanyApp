88137c189970d12d3e2db62d27c347fc5655d2df
김승원 <bananamilk420@gmail.com>
2025-01-08 11:47:09 -0600

    feat: :sparkles: `인재채용`>`채용링크` page redirects to https://hanjinpl.career.rivers.co.kr/
    Browser 뒤로가기 기능 이상 및 페이지 비주얼 문제. Nginx로 아예 proxy 하는걸 권장.

--- frontend/src/pages/인재채용/채용링크.tsx
+++ frontend/src/pages/인재채용/채용링크.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import '../Font&Color.css';
 import '../thumbnail&details.css';
 import '../divs.css';
 
 
 export default function 채용링크() {
-  return (
-    <>채용링크</>
-  );
+  useEffect(() => {
+    window.location.href = "https://hanjinpl.career.rivers.co.kr/";
+  });
+
+  return <h5>Redirecting...</h5>;
 }
